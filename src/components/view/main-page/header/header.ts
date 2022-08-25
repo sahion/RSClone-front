564cc843987d0a9034224bc578a243becfe9f915
jazzzrabbit <jazzzrabbit@gmail.com>
2022-08-25 09:46:39 +0300

    refactor: make single header class with styles for both main and user pages

--- src/components/view/main-page/header/header.ts
+++ src/components/view/main-page/header/header.ts
@@ -1,4 +1,5 @@
 import logo from '../../../assets/img/Logo.png';
+import vladimirAva from '../../../assets/img/Владимир.png';
 
 export default class Header {
   wrapper: HTMLElement;
@@ -9,28 +10,63 @@ export default class Header {
 
   getHeader(): string {
     return `
-            <div class="container">
-              <div class="header__wrapper">
-                <div class="header__logo">
-                  <a href="#"><img class="header__logo-img" src=${logo} alt="Logo"></a>
-                </div>
-                <nav class="header__nav">
-                  <ul class="header__nav-list">
-                    <li class="header__nav-item"><a href="#requests">Успей помочь</a></li>
-                    <li class="header__nav-item"><a href="#thanks">Копилка добрых дел</a></li>
-                    <li class="header__nav-item"><a href="#info">Как это работает?</a></li>
-                  </ul>
-                </nav>
-                <div class="header__btns">
-                  <button class="btn header__login-btn" id="login">Вход</button>
-                  <button class="btn header__register-btn" id="register">Регистрация</button>
-                </div>
-              </div>
-            </div>`;
+    <div class="container">
+      <div class="header__wrapper">
+        <div class="header__logo">
+          <a href="#"><img class="header__logo-img" src=${logo} alt="Logo"></a>
+        </div>
+        <nav class="header__nav">
+          <ul class="header__nav-list">
+            <li class="header__nav-item"><a href="#requests">Успей помочь</a></li>
+            <li class="header__nav-item"><a href="#thanks">Копилка добрых дел</a></li>
+            <li class="header__nav-item"><a href="#info">Как это работает?</a></li>
+          </ul>
+        </nav>
+        <div class="header__btns">
+          <button class="btn header__login-btn" id="login">Вход</button>
+          <button class="btn header__register-btn" id="register">Регистрация</button>
+        </div>
+      </div>
+    </div>`;
   }
 
-  render() {
-    this.wrapper.innerHTML = this.getHeader();
+  getUserHeader(): string {
+    return `
+    <div class="container">
+      <div class="header__wrapper">
+        <div class="header__logo">
+          <img class="header__logo-img" src=${logo} alt="Logo">
+        </div>
+        <nav class="header__nav">
+          <ul class="header__nav-list">
+            <li class="header__nav-item">Здравствуйте, Владимир!</li>
+            <li class="header__nav-item header__avatar">
+              <img class="header__avatar-img" src=${vladimirAva} alt="Avatar">
+              <ul class="header__submenu">
+                <li class="header__submenu-item"><a href="">Просмотреть</a></li>
+                <li class="header__submenu-item"><a href="">Редактировать</a></li>
+                <li class="header__submenu-item header__logout">
+                  <a href="">Выйти</a>          
+                </li>
+              </ul>
+            </li>
+          </ul>
+        </nav>          
+      </div>
+    </div>`;
+  }
+
+  render(page = 'main'): HTMLElement {
+    switch (page) {
+      case ('main'): {
+        this.wrapper.innerHTML = this.getHeader();
+        break;
+      }
+      case ('user'): {
+        this.wrapper.innerHTML = this.getUserHeader();
+        break;
+      }
+    }
     this.wrapper.classList.add('header');
     return this.wrapper;
   }
